e5a4ea0ae3fb1e913d55bff708de890f1259ee5e
Jarrod Wandrey <37374270+JarrodWandrey@users.noreply.github.com>
2022-10-14 15:12:50 -0500

    added pickmany option
    allows for the user to now be able to pick multiple items from the data rather than just one at a time

--- DnD_NPCGen/main.py
+++ DnD_NPCGen/main.py
@@ -1,6 +1,8 @@
+from faulthandler import disable
+from textwrap import wrap
 import tkinter
 import getdata,writefile,pullnpc
-from tkinter import Label, Tk, Canvas, Button, ttk, messagebox
+from tkinter import Label, Tk, Canvas, Button, Widget, ttk, messagebox, Entry
 import os
 
 #CREATE WINDOW FOR PROGRAM
@@ -8,7 +10,7 @@ root = Tk() #creates window object
 root.title("DnD Npc Generator") #sets window title
 root.configure(width=800, height=400) #set dimensions of window
 
-canvas = Canvas(root,width=500,height=500,bg='darkgrey')#create a canvas to put text on
+canvas = Canvas(root,width=600,height=500,bg='darkgrey')#create a canvas to put text on
 canvas.create_text(250,50,text="Dungeon and Dragons NPC Generator",font=('Helvetica 19 bold'))#Text placed on the opened window
 canvas.pack()
 
@@ -19,13 +21,12 @@ def getandlook():
     tkinter.messagebox.showinfo("Complete!","NPCs Generated!")
     pullnpc.usedbin.clear()
 
-text = tkinter.Text(root,height=20,width=45)#create text box for npc data
+text = tkinter.Text(root,height=20,width=55, wrap=tkinter.WORD)#create text box for npc data
 text.place(x=120,y=90)
 text.config(state='disabled')#disables ability to manually write in text box
 
 #create a output of npc details on the window when 'pick' button is pressed
 def npcprint():
-    #label = tkinter.Label(root, text=pullnpc.pull_npc())
     text.config(state='normal')#changes state of text box to be written into
     text.delete('1.0', 'end')#clears the frist character to the last character of text within the box (where '1.0' is the first character and 'end' is the last character)
     text.insert('insert', pullnpc.pull_npc())#insert npc data to the textbox
@@ -36,6 +37,33 @@ def clearall():
     text.config(state='normal')
     text.delete('1.0', 'end')
     text.config(state='disabled')
+
+
+# #function to pull multiple npcs
+e = tkinter.Entry(root, width=5)#create entry box for numbers
+e.pack()
+e.place(x=60, y=155)
+
+
+def pickmany():
+    val = int(e.get()) #casts the entry box input into an integer
+    while val != 0: #loop to print out multiple npcs
+        text.config(state='normal')#changes state of text box to be written into
+        text.insert('insert', pullnpc.pull_npc())
+        text.insert('insert', '\n')
+        val = val - 1
+    text.config(state='disabled')
+
+
+#funtion to show pickmany function
+def showmany():
+    text.config(state='normal')#changes state of text box to be written into
+    text.insert('insert', pickmany())#insert npc data to the textbox
+    text.config(state='disabled')#close the textbox for edition once finished
+
+#create button to pick many
+pickmore = Button(root, text="Pick :", command=showmany, highlightbackground='lightgray')
+pickmore.place(x=20, y=150)
 
 #creates a button to generate the text file of npcs
 generate = Button(root, text="generate!", command= getandlook ,highlightbackground='lightgray')
@@ -51,7 +79,7 @@ close.place(x=20,y=390)
 
 #Button to clear generated text from window
 cleartext = Button(root, text='Clear', command=clearall, highlightbackground='lightgray')
-cleartext.place(x=20, y=170)
+cleartext.place(x=20, y=200)
 
 root.mainloop()
 os.remove('npc.txt')#removes previous loaded file from last generate
