0ed31181616e2f245d46ab556658b32030dc2b08
Jarrod Wandrey <37374270+JarrodWandrey@users.noreply.github.com>
2022-11-15 22:02:14 -0600

    comments

--- DnD_NPCGen/writefile.py
+++ DnD_NPCGen/writefile.py
@@ -1,6 +1,6 @@
 import settings
 
-def write_file():
+def write_file(): #function to write in all gen'd npc into the file
     with open("npc.txt", "w") as f:
         for x in settings.NPCs:
             f.write(x + '\n')
